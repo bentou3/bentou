14f92a1a286c80fbf3fa41c0b038a6516065fc46
bentou123 <3324673551@qq.com>
2023-07-19 21:49:15 +0800

    homework

---
+++ homework/homework2/homework2/homework.c
@@ -0,0 +1,92 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i < 100; i++)
+//	{
+//		if (i % 3 == 0)
+//		{
+//			printf("%d\n", i);
+//		}
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int x, y, z;
+//	int tmp=0;
+//	scanf("%d", &x);
+//	scanf("%d", &y);
+//	scanf("%d", &z);
+//	if (x < y)
+//	{
+//		tmp = x;
+//		x = y;
+//		y = tmp;
+//	}
+//	 if (x < z)
+//	{
+//		tmp = x;
+//		x = z;
+//		z=tmp;
+//	}
+//	 if (x < y)
+//	 {
+//		 tmp = x;
+//		 x = y;
+//		 y = tmp;
+//	 }
+//	 printf("%d %d %d", x, y, z);
+//	 return 0;
+//}
+
+
+//求100到200间所有素数
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (i = 100; i < 200; i++)
+//	{
+//		for (j = 2; j < i; j++)
+//		{
+//			if (i % j == 0)
+//			{
+//				break;
+//			}
+//		}
+//		if (i == j)
+//		{
+//			printf("%d\n", i);
+//		}
+//	}
+//	return 0;
+//}
+// 
+//打印1000到2000年间的闰年
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1000; i <= 2000; i++)
+//	{
+//		if (i % 4 == 0)
+//		{
+//			printf("%d\n", i);
+//		}
+//	}
+//	return 0;
+//}
+
+//给定两个数，求这两个数的最大公约数
+//例如：
+//输入：20 40
+//输出：20
+int main()
+{
+	int i = 0;
+	int j = 0;
+
+}
