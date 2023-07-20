0d3afb4a230748fa622cc05851e4d1e17cec2565
bentou123 <3324673551@qq.com>
2023-07-20 19:33:24 +0800

    小小

---
+++ 23-7-20/23-7-20/test.c
@@ -0,0 +1,37 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//	int a = 0, b = 0;
+//	for (a = 1, b = 1; a <= 100; a++)
+//	{
+//		if (b >= 20) break;
+//		if (b % 3 == 1)
+//		{
+//			b = b + 3;
+//			continue;
+//		}
+//		b = b - 5;
+//	}
+//	printf("%d\n", a);
+//	return 0;
+//}
+//编写程序数一下 1到 100 的所有整数中出现多少个数字9
+//int main()
+//{
+//	int i = 0;
+//	int num = 1;
+//	for (i = 1; i <= 100; i++)
+//	{
+//		if(i%10==9)//个位数字上9个数
+//		{
+//			num++;
+//		}
+//		if(i/10==9)//十位数字上9个数
+//		{
+//			num++;
+//		}
+//	}
+//	printf("%d", num);
+//	return 0;
+//}
