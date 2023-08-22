bf66aec44fc44c4bcbcc7176974480459543ce50
bentou123 <3324673551@qq.com>
2023-08-22 23:28:14 +0800

    666

---
+++ 2023-8-223/2023-8-223/2023-8-22.c
@@ -0,0 +1,17 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main() {
+    printf("************ 开心的一天 ************\n");
+    printf("今天是个美好的一天！\n");
+    printf("笑一个，世界会更美好！\n");
+    printf("你是最棒的，相信自己！\n");
+    printf("每天进步一点点，未来会更加辉煌！\n");
+    printf("用心感受生活中的美好瞬间！\n");
+    printf("保持微笑，别让烦恼占据你的心情！\n");
+    printf("做自己喜欢的事情，享受每一天！\n");
+    printf("希望你有一个开心而愉快的一天！\n");
+    printf("**********************************\n");
+
+    return 0;
+}
