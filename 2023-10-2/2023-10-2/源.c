67375a9d576906b3554e76b0a72a6fcedcbe172d
bentou123 <3324673551@qq.com>
2023-10-02 21:53:45 +0800

    庆国庆

---
+++ 2023-10-2/2023-10-2/源.c
@@ -0,0 +1,34 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+void printBanner() {
+    printf("*********************\n");
+    printf("*                   *\n");
+    printf("*   庆 国 庆 节      *\n");
+    printf("*                   *\n");
+    printf("*********************\n");
+}
+
+void printFireworks() {
+    printf("         .-\"\"\"\"-.\n");
+    printf("        '        '\n");
+    printf("        | 火    |\n");
+    printf("        |  花   |\n");
+    printf("        |   绽  |\n");
+    printf("        |    放 |\n");
+    printf("        |     致|\n");
+    printf("        | 胜    | \n");
+    printf("        |     庆|\n");
+    printf("        |       |\n");
+    printf("        '-.....-'\n");
+}
+
+int main() {
+    printBanner();
+    printf("\n");
+    printf("祝您国庆节快乐！\n");
+    printf("\n");
+    printFireworks();
+
+    return 0;
+}
